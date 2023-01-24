fc2d4abe03c29f741f8cb7623341b248d9571373
Elmiven <elmiven@hotmail.com>
2023-01-24 17:07:23 +0300

    add Header Technologies modules (6-9 lesson)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
 
-function App() {
+
+import Technologies from './Technologies';
+
+
+
+const App = () => {
   return (
-    <div className="App">
-     Hello, samurai! Let's go!
+    <div>
+      {/* Hello, samurai! Let's go! */}
+      <Header />
+      <Technologies />
     </div>
   );
 }
+
+
+
+
+
+
 
 export default App;
---
+++ src/Technologies.tsx
@@ -0,0 +1,17 @@
+const Technologies = () => {
+    return (
+      <div className = "App" >
+        <ul>
+          <li>cms</li>
+          <li>html</li>
+          <li>js</li>
+          <li>react</li>
+        </ul>
+  </div >
+  
+  )
+   
+  }
+
+
+  export default Technologies;
