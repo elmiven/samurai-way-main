d2a33a2df4524dfa658fb5ce343ed398a20fc70a
Elmiven <elmiven@hotmail.com>
2023-02-24 02:44:14 +0300

    lsn25 map

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -15,30 +15,58 @@ const DialogItem = (props: any) => {
 
 const MessageItem = (props: any) => {
     return (
-        <div className={s.message}>{ props.message}</div>
+        <div className={s.message}>{props.message}</div>
     )
 }
 
 
-const Dialogs = (props: any) => {
+let dialogs = [
+    { id: 1, name: 'Mitka' },
+    { id: 2, name: 'Mikha' },
+    { id: 3, name: 'Petka' },
+    { id: 4, name: 'Petka' },
+    { id: 5, name: 'Lenka' },
+]
 
+
+let messages = [
+    { id: 1, message: 'Hey' },
+    { id: 2, message: 'Ho' },
+    { id: 3, message: 'Les Go!' },
+]
+
+
+//lesson 25.map
+let dialogElements = dialogs
+    .map(dialog => <DialogItem name={dialog.name} id={dialog.id} />);
+//lesson 25.map
+let messageElements = messages
+    .map(messageEl => <MessageItem message={messageEl.message} />);
+
+
+const Dialogs = (props: any) => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
+                {dialogElements}
+
                 {/* <div className={s.dialog}>
                     <NavLink to='/dialogs/1'>Mitka</NavLink>
-                </div> */}
-                <DialogItem name="Mitka " id='1' />
+                </div>
+                <DialogItem name={dialogs[0].name} id={dialogs[0].id} />
+                <DialogItem name={dialogs[2].name} id={dialogs[1].id} />
                 <DialogItem name="Mikha " id='2' />
                 <DialogItem name="Petka " id='3' />
-                <DialogItem name="Svetka " id='4' />
-                <DialogItem name="Lenka " id='5' />
+                <DialogItem name="Petka " id='4' />
+                <DialogItem name="Lenka " id='5' /> */}
             </div>
 
             <div className={s.messages}>
-                <MessageItem message="Hey"/>
-                <MessageItem message="Ho"/>
-                <MessageItem message="Lets go!"/>
+                {messageElements}
+                {/* <MessageItem message={messages[0].message} />
+                <MessageItem message={messages[1].message} />
+                <MessageItem message="Ho" />
+                <MessageItem message="Lets go!" /> */}
             </div>
         </div>
 
--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,6 +1,18 @@
 import Post from './Post/Post';
 import s from './MyPosts.module.css'
 
+
+
+let posts = [
+  {id: 1, message: 'Hi! HRU?', likescount: 5 },
+  {id: 2, message: 'Hello I\'m fine!', likescount: 12},
+  ]
+
+
+//lesson 25.map
+let postElements = posts.map( post => <Post message={post.message} likecount={post.likescount} />)
+
+
 const MyPosts = () => {
   return (
     <div className={s.postsBlock}>
@@ -12,9 +24,12 @@ const MyPosts = () => {
         </div>
       </div>
 
+
+
       <div className={s.posts}>
-        <Post message="Hi! HRU?" likecount='5' />
-        <Post message="Hello! I'm fine!" likecount='15' />
+        {postElements}
+        {/* <Post message={posts[0].message} likecount={posts[0].likescount} />
+        <Post message="Hello! I'm fine!" likecount='15' /> */}
       </div>
 
 
